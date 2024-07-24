f4c2c1f1ac703967e3a2a2f4afae4c46b58623f1
Nikolai <n_prusov@inbox.ru>
2024-07-24 22:26:51 +0300

    js cods wars

--- index.js
+++ index.js
@@ -25,8 +25,64 @@
     // TODO
     }  */
 
+  /*   function removeChar(str){
+      let result ='';
+      let l = str.length;
+      for (i=1; i <= (l-2); i++) {
+        result = result + str[i];
+      };
+    return result;
+    };
+ */
+    /* function noSpace(x){
+      let result = '';
+      for (i = 0; i <= (x.length-1); i++){
+        if (x[i] === ' ') {
+          result = result;
+        } else {
+            result = result + x[i];
+          };
+      };
+      return result;
+    };
+ */
 
 
-  let z = digits (9007199254740991);
+/* function solution(str, ending){
+  return str.endsWith(ending);
+} 
+ */
 
-  console.log (z);
+/* function repeatStr (n, s) {
+  return s.repeat(n);
+} */
+/* 
+  function finalGrade (exam, projects) {
+    if (exam>90 || projects>10) {return 100};
+    if (exam>75 || projects>=5) {return 90};
+    if (exam>50 && projects>=2) {return 75};
+    return 0;
+  } 
+ */
+
+  function switchItUp(number){
+    switch (number) {
+      case 0: return 'Zero';
+      case 1: return 'One';
+      case 2: return 'Two';
+      case 3: return 'Three';
+      case 4: return 'Four';
+      case 5: return 'Five';
+      case 6: return 'Six';
+      case 7: return 'Seven';
+      case 8: return 'Eight';
+      case 9: return 'Nine';
+    };
+  }
+
+
+
+
+let z = switchItUp (2);
+
+console.log (z);
