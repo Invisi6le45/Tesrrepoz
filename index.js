aca814a08393bc65cc53d8d43978aa19db698fbe
Nikolai <Invisi6le.prusovndm@inbox.ru>
2024-07-24 16:57:18 +0300

    js

---
+++ index.js
@@ -0,0 +1,32 @@
+/* function fakeBin(x) {
+    let result = '';
+    let l = x.length;
+    console.log (l);
+    for (i = 0; i<=l; i++ ) {
+      if ((x[i]) < '5') {
+        result = result + '0'; 
+        } else if (x[i] >= '5') {result = result + '1'};
+    }
+    return result;
+  } */
+
+
+/*   function areaLargestSquare(r) {
+    let a = (2 * r) / Math.sqrt(2);
+    let result = a** 2;
+    return Math.round(result); //code here
+  } */
+
+/* function digits(n) {
+    let text = n.toString();
+    console.log (text);
+    let result = text.length;
+    return result;
+    // TODO
+    }  */
+
+
+
+  let z = digits (9007199254740991);
+
+  console.log (z);
